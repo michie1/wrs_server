6088ba8c5deb038ba8043c3528f8f0029e91b7e5
Michiel <git@fastfox.nl>
2016-08-18 22:18:36 +0200

    get riders from api which are stored in redis

--- server.js
+++ server.js
@@ -1,24 +1,59 @@
 let http = require('http');
+let express = require('express');
+let bodyParser = require('body-parser');
+
 let redis = require('redis');
 let client = redis.createClient();
 
-let server = http.createServer((req, res) => {
-    res.writeHead(200);
+let app = express();
 
-    if (req.url !== '/favicon.ico') {
-        client.get('value', (err, reply) => {
-            console.log(reply);
-            client.set('value', parseInt(reply) + 1);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// Get rider data 
+// @param {string} riderSlug The slug of a rider
+// @return {Promise} 
+// @resolve {object} The data of the rider
+function getRider(riderSlug) {
+    return new Promise((resolve, reject) => {
+        client.get('rider:' + riderSlug, (err, rider) => {
+            resolve(JSON.parse(rider));
         });
-    }
+    });
+}
 
-    res.end('hello Http');
+app.get('/riders', function(req, res) {
+    // Get set of riders
+    client.smembers('riders', (err, ridersReply) => {
+        let p = []; // For each rider a promise
+
+        // Get rider data for each rider
+        ridersReply.map((riderSlug) => {
+            p.push(getRider(riderSlug));
+        });
+
+        // When all promises resolved, output data
+        Promise.all(p).then(riders => {
+            res.json(riders);
+        });
+    });
 });
-    
 
-client.on('connect', function() {
-    console.log('connect');
-    client.set('value', 0);
+
+app.get('/setup', (req, res) => {
+    // riders
+    client.sadd('riders', 'michiel');
+    client.sadd('riders', 'henk');
+
+    client.set('rider:michiel', '{"name": "Michiel"}');
+    client.set('rider:henk', '{"name": "Henk"}');
+
+
+    res.json({message: 'setup'});
 });
 
-server.listen(8080);
+app.listen(8080);
