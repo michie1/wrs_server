c3cc2e88f1caf7e948492ac21508a05658b7aa72
Michiel <git@fastfox.nl>
2016-08-21 09:29:23 +0200

    use baseUrl in test/api

--- test/api.js
+++ test/api.js
@@ -1,44 +1,96 @@
-casper.test.begin('WRS API', 7, function(test) {
-    casper.start('http://localhost:8080', function() {
+require('es6-set/implement');
+Object.is = require('object-is');
+
+var baseUrl = 'http://localhost:8080';
+
+casper.test.begin('WRS API', 15, function(test) {
+    casper.start(baseUrl, function() {
         casper.then(function() {
             test.assertEquals(this.getPlainText(), '{"message":"wrs api"}', '/');
         });
 
-        casper.thenOpen('http://localhost:8080/setupTest', function() {
+        casper.thenOpen(baseUrl + '/setupTest', function() {
             test.assertEquals(this.getPlainText(), 
                               '{"message":"setupTest"}', 
-                              '/setupTest');
+                              this.getCurrentUrl().replace(baseUrl, ''));
         });
 
-        casper.thenOpen('http://localhost:8080/riders', function() {
-            test.assertEquals(this.getPlainText(), 
-                              '[{"name":"Henk"},{"name":"Michiel"}]',
-                              '/riders');
+        casper.thenOpen(baseUrl + '/riders', function() {
+            // API returns array in an undefined order
+            var expectedRiders = new Set([
+                '{"name":"Michiel"}',
+                '{"name":"Henk"}'
+            ]); 
+            var riders = JSON.parse(this.getPlainText());
+            test.assertEquals(riders.length, 2, 'Two riders in /riders');
+
+            riders.map(function(rider) {
+                test.assert(expectedRiders.has(JSON.stringify(rider)), rider.name + ' in /riders');
+            });
         });
 
-        casper.thenOpen('http://localhost:8080/races', function() {
-            test.assertEquals(this.getPlainText(), 
-                              '[{"name":"Gouden Pijl Emmen","date":"2016-08-11","type":"criterium","report":"verslag"},{"name":"Ronde van de Lier","date":"2016-08-11","type":"criterium","report":"verslag"}]', 
-                              '/races');
+        casper.thenOpen(baseUrl + '/races', function() {
+            var expectedRaces = new Set([
+                '{"name":"Gouden Pijl Emmen","date":"2016-08-11","type":"criterium","report":"verslag"}',
+                '{"name":"Ronde van de Lier","date":"2016-08-11","type":"criterium","report":"verslag"}'
+            ]);
+
+            var races = JSON.parse(this.getPlainText());
+            test.assertEquals(races.length, 2, 'Two races in /races');
+
+            races.map(function(race) {
+                test.assert(expectedRaces.has(JSON.stringify(race)), race.name + ' in /races');
+            });
         });
 
-        casper.thenOpen('http://localhost:8080/races/2016-08-11/ronde-van-de-lier', function() {
+        casper.thenOpen(baseUrl + '/races/2016-08-11/ronde-van-de-lier', function() {
             test.assertEquals(this.getPlainText(), 
                               '{"name":"Ronde van de Lier","date":"2016-08-11","type":"criterium","report":"verslag"}',
                              '/races/2016-08-11/ronde-van-de-lier');
+                              this.getCurrentUrl().replace(baseUrl, '');
         });
 
-        casper.thenOpen('http://localhost:8080/riders/michiel', function() {
+        casper.thenOpen(baseUrl + '/riders/michiel', function() {
             test.assertEquals(this.getPlainText(), 
                               '{"name":"Michiel"}',
                              '/riders/michiel');
+                              this.getCurrentUrl().replace(baseUrl, '');
         });
 
-        casper.thenOpen('http://localhost:8080/results/michiel/2016-08-11/ronde-van-de-lier', function() {
+        casper.thenOpen(baseUrl + '/results/michiel/2016-08-11/ronde-van-de-lier', function() {
             test.assertEquals(this.getPlainText(), 
                               '12',
-                             '/results/michiel/2016-08-11/ronde-van-de-lier');
+                              this.getCurrentUrl().replace(baseUrl, ''));
         });
+
+        casper.thenOpen(baseUrl + '/results/michiel', function() {
+            var me = this;
+            var expectedResults = new Set([
+                '{"result":"12","race":{"name":"Ronde van de Lier","slug":"ronde-van-de-lier","date":"2016-08-11"}}'
+            ]);
+
+            var results = JSON.parse(this.getPlainText());
+            test.assertEquals(results.length, 1, 'One result in /results/michiel');
+            
+            results.map(function(result) {
+                test.assert(expectedResults.has(JSON.stringify(result)), me.getCurrentUrl().replace(baseUrl, ''));
+            });
+        });
+
+       casper.thenOpen(baseUrl + '/results/henk', function() {
+            var me = this;
+            var expectedResults = new Set([
+                '{"result":"21","race":{"name":"Gouden Pijl Emmen","slug":"gouden-pijl-emmen","date":"2016-08-11"}}'
+            ]);
+
+            var results = JSON.parse(this.getPlainText());
+            test.assertEquals(results.length, 1, 'One result in /results/henk');
+            
+            results.map(function(result) {
+                test.assert(expectedResults.has(JSON.stringify(result)), me.getCurrentUrl().replace(baseUrl, ''));
+            });
+        });
+
 
     }).run(function() {
         test.done();
