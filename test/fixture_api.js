8544d1af9f3ff6e70032fe2523af4facd6e53d18
Michiel <git@fastfox.nl>
2016-08-25 15:59:28 +0200

    Added missing data for test fixture

--- test/fixture_api.js
+++ test/fixture_api.js
@@ -28,11 +28,14 @@ router.get('/', (req, res) => {
     client.set('race:2016-08-11:gouden-pijl-emmen', '{"name": "Gouden Pijl Emmen", "date": "2016-08-11", "type": "criterium", "report": "verslag"}');
 
     // results
-    client.set('result:michiel:2016-08-11:ronde-van-de-lier', 12);
+    client.set('result:michiel:2016-08-11:ronde-van-de-lier', 21);
     client.set('result:henk:2016-08-11:gouden-pijl-emmen', 31);
 
-    client.sadd('results:michiel', '{"result":"12","race":{"name":"Ronde van de Lier","slug":"ronde-van-de-lier","date":"2016-08-11"}}');
-    client.sadd('results:henk', '{"result":"21","race":{"name":"Gouden Pijl Emmen","slug":"gouden-pijl-emmen","date":"2016-08-11"}}');
+    client.sadd('results:michiel', '{"result":"21","race":{"name":"Ronde van de Lier","slug":"ronde-van-de-lier","date":"2016-08-11"}}');
+    client.sadd('results:henk', '{"result":"31","race":{"name":"Gouden Pijl Emmen","slug":"gouden-pijl-emmen","date":"2016-08-11"}}');
+                
+    client.sadd('results:2016-08-11:gouden-pijl-emmen', '{"result":"31","rider":{"name":"Henk","slug":"henk"}}');
+    client.sadd('results:2016-08-11:ronde-van-de-lier', '{"result":"21","rider":{"name":"Michiel","slug":"michiel"}}');
 
     res.json({message: 'setupTest'});
 });
